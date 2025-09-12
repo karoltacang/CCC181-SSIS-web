38b5f9f5264f4823160b503fe9987ef42d1fcf05
Karol <karol.tacangg@gmail.com>
2025-09-12 16:27:00 +0800

    applied styling to pages

---
+++ react-app/src/pages/Programs.jsx
@@ -0,0 +1,12 @@
+import DataPage from "../components/DataPage";
+
+const programData = [
+  { code: "CS101", name: "Computer Science", college: "CCS" },
+  { code: "ENG201", name: "English", college: "CASS" },
+];
+
+const programColumns = ["Code", "Name", "College"];
+
+export default function Programs() {
+  return <DataPage title="Programs" data={programData} columns={programColumns} />;
+}
---
+++ react-app/src/pages/Students.jsx
@@ -0,0 +1,12 @@
+import DataPage from "../components/DataPage";
+
+const studentData = [
+  { id: "2021-0107", firstname: "John", lastname: "Doe", course: "CS", year: "1st", gender: "Male" },
+  { id: "2021-0108", firstname: "Jane", lastname: "Smith", course: "Math", year: "2nd", gender: "Female" },
+];
+
+const studentColumns = ["ID", "First Name", "Last Name", "Course", "Year", "Gender"];
+
+export default function Students() {
+  return <DataPage title="Students" data={studentData} columns={studentColumns} />;
+}
