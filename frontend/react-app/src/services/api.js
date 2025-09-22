6e020cab335636e74cd3c7946aa02a3d2588a18d
Mib <karol.tacangg@gmail.com>
2025-09-22 14:12:39 +0800

    feat: add axios client with colleges, programs, and students CRUD endpoints

---
+++ frontend/react-app/src/services/api.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+const API_URL = '/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Colleges API
+export const collegesAPI = {
+  getAll: () => api.get('/colleges'),
+  getByCode: (code) => api.get(`/colleges/${code}`),
+  create: (data) => api.post('/colleges', data),
+  update: (code, data) => api.put(`/colleges/${code}`, data),
+  delete: (code) => api.delete(`/colleges/${code}`),
+};
+
+// Programs API
+export const programsAPI = {
+  getAll: (collegeCode) => {
+    const params = collegeCode ? { college_code: collegeCode } : {};
+    return api.get('/programs', { params });
+  },
+  getByCode: (code) => api.get(`/programs/${code}`),
+  create: (data) => api.post('/programs', data),
+  update: (code, data) => api.put(`/programs/${code}`, data),
+  delete: (code) => api.delete(`/programs/${code}`),
+};
+
+// Students API
+export const studentsAPI = {
+  getAll: (programCode, search) => {
+    const params = {};
+    if (programCode) params.program_code = programCode;
+    if (search) params.search = search;
+    return api.get('/students', { params });
+  },
+  getById: (id) => api.get(`/students/${id}`),
+  create: (data) => api.post('/students', data),
+  update: (id, data) => api.put(`/students/${id}`, data),
+  delete: (id) => api.delete(`/students/${id}`),
+};
+
+export default api;
