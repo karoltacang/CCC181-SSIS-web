1b102f9fcbdb532722d2db31309fbc98d884ea90
Mib <karol.tacangg@gmail.com>
2025-09-22 11:58:25 +0800

    fix: changed var naming

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,5 +5,5 @@ class Config:
     DB_HOST = getenv('DB_HOST')
     DB_PORT = getenv('DB_PORT', '5432')
     DB_NAME = getenv('DB_NAME')
-    DB_USER = getenv('DB_USER')
+    DB_USER = getenv('DB_USERNAME')
     DB_PASSWORD = getenv('DB_PASSWORD')
