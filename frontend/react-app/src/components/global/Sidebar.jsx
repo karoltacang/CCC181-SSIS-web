191e27ed43429ea4cfc439246a2682620744b695
Karol Maeve Tacang <karolmaeve.tacang@g.msuiit.edu.ph>
2025-09-27 18:43:08 +0800

    refactor: moved sidebar under global folder

---
+++ frontend/react-app/src/components/global/Sidebar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="logo">SSIS</div>
+      <ul>
+        <li>
+          <Link to="/students">Students</Link>
+        </li>
+        <li>
+          <Link to="/programs">Programs</Link>
+        </li>
+        <li>
+          <Link to="/colleges">Colleges</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
