adea2e8e96bd64d57e4ef94bd9a48d077dfe2cd5
Karol Maeve Tacang <karolmaeve.tacang@g.msuiit.edu.ph>
2025-11-05 00:26:29 +0800

    feat: enable pooling

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,18 +1,25 @@
 import psycopg2
+from psycopg2 import pool
 from psycopg2.extras import RealDictCursor
 from flask import g
 from app.config import Config
 
+pg_pool = None
+
 def get_db_connection():
-  conn = psycopg2.connect(
-  host=Config.DB_HOST,
-  port=Config.DB_PORT,
-  database=Config.DB_NAME,
-  user=Config.DB_USER,
-  password=Config.DB_PASSWORD,
-  sslmode='require'
-  )
-  return conn
+  global pg_pool
+  if pg_pool is None:
+    pg_pool = psycopg2.pool.ThreadedConnectionPool(
+      minconn=1,
+      maxconn=20,
+      host=Config.DB_HOST,
+      port=Config.DB_PORT,
+      database=Config.DB_NAME,
+      user=Config.DB_USER,
+      password=Config.DB_PASSWORD,
+      sslmode='require'
+    )
+  return pg_pool.getconn()
 
 def get_db():
   if 'db' not in g:
@@ -22,7 +29,10 @@ def get_db():
 def close_db(e=None):
   db = g.pop('db', None)
   if db is not None:
-    db.close()
+    if pg_pool:
+      pg_pool.putconn(db)
+    else:
+      db.close()
 
 def init_app(app):
   app.teardown_appcontext(close_db)
